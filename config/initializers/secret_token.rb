450806843776f505fa4a48856567fbf29d889cc5
Apprentice <apprentice@dbc-chicago-008.local>
2014-03-31 14:57:15 -0500

    Initial commit - deleted user and password from config/database.yml and added rails_12factor gem for heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CraigsList::Application.config.secret_key_base = '300b537753aa13efe80d3699f6d93190ce1a1d7636e2b180f9d9d594d5f0a8f6f09a228c55f849181d585c537334bb8f483183d98e21ef3dca6a21b312fe6fc0'
