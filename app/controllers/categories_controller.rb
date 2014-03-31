e5dd900235ec1bab9f0ff7aefb6267d25ec166b9
Apprentice <apprentice@dbc-chicago-008.local>
2014-03-31 16:15:18 -0500

    added ability to add categories - index/new/create/show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,27 @@
 class CategoriesController < ApplicationController
+
+
   def index
     @categories = Category.all
   end
+
+  def new
+    @categories = Category.new
+  end
+
+  def create
+    # render text: params[:categories].inspect
+    @categories = Category.new(params[:categories].permit(:name))
+    @categories.save
+    redirect_to @categories
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:name)
+    end
 end
