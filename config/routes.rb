a2ad27b2ec2b98e824707c1451755b692b8d736c
Apprentice <apprentice@dbc-chicago-008.local>
2014-03-31 15:16:55 -0500

    generated Categories controller, generated Category model, migrated categories table

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 CraigsList::Application.routes.draw do
 
+  get "categories/index"
   resources :categories
 
-  root to: "category#index"
+  root to: "categories#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
