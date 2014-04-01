1967365a164a7a7327d9183e28fd8d0aab394e2a
Apprentice <apprentice@dbc-chicago-008.local>
2014-03-31 19:49:24 -0500

    working MVP Craigslist Jr - meets challenge objectives

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,11 @@
 class PostsController < ApplicationController
+  def create
+    @category = Category.find(params[:category_id])
+    @post = @category.posts.create(params[:post].permit(:title, :description, :category_id))
+    redirect_to category_path(@category)
+  end
+
+  def edit
+    @post = Post.find(params(:id))
+  end
 end
